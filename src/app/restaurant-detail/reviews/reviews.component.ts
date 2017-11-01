27d8282cf30f49a37e442e12358c95e5d3350a5a
Fabiane 000926 <fabiane.paiva@leucotron.com.br>
2017-11-01 17:56:44 -0200

    first commit

---
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,23 @@
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { RestaurantsService } from './../../restaurants/restaurants.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewsComponent implements OnInit {
+
+  reviews: Observable<any>
+
+  constructor(private restaurantsService:RestaurantsService,
+              private activatedRoute:ActivatedRoute) { }
+
+
+  ngOnInit() {
+    this.reviews =  this.restaurantsService.reviewsOfRestaurant(
+                    this.activatedRoute.parent.snapshot.params['id']);
+  }
+
+}
